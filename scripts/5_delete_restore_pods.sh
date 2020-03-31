3ee55c75def78bced84e7c54201d31d2feab4524
Adrian Kosmaczewski <adrian@vshn.ch>
2020-03-31 17:40:58 +0200

    Adapted demo to backup MariaDB using command
    (cherry picked from commit 2541e11487a812a8082d6ac64f9f01a8f756cf30)

--- scripts/5_delete_restore_pods.sh
+++ scripts/5_delete_restore_pods.sh
@@ -7,8 +7,5 @@ source scripts/environment.sh
 # Set kubectl context
 export KUBECONFIG="$(k3d get-kubeconfig --name='k3s-default')"
 
-# MariaDB
-restic snapshots --json --last --path /data/mariadb-pvc | python scripts/customize.py mariadb | kubectl delete -f -
-
 # WordPress
 restic snapshots --json --last --path /data/wordpress-pvc | python scripts/customize.py wordpress | kubectl delete -f -
