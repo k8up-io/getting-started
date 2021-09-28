ab5e4aeed79742a1824c0c6ad5fad94526153a19
Adrian Kosmaczewski <adrian.kosmaczewski@vshn.ch>
2021-09-28 16:32:06 +0200

    Small typos

--- scripts/6_stop.sh
+++ scripts/6_stop.sh
@@ -3,10 +3,15 @@
 # Set kubectl context
 export KUBECONFIG="./scripts/exoscale-sks.kubeconfig"
 
+# Remove Kubernetes objects
 kubectl delete -k mariadb
 kubectl delete -k wordpress
 kubectl delete -k secrets
 
+# Remove SKS cluster
 cd scripts
 terraform destroy
 cd ..
+
+# Delete Exoscale Object Storage bucket
+exo storage rb sos://backups-getting-started-repo --force --recursive
